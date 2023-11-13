e4484df675d822fc15a323d1269278d8b90466e1
Magomed <magomed.alibekov.07@list.ru>
2023-11-13 14:23:20 +0300

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+class HomeController extends Controller
+{
+  public function [SpaseX]
+
+echo 'hello world'
+}
